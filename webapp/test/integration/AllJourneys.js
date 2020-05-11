9e5b7d2d75adcee0cea2e830aa4dea0bd8215e88
mbrly <brulinski.michal@gmail.com>
2020-05-11 12:14:21 +0000

    Test Commit #1

---
+++ webapp/test/integration/AllJourneys.js
@@ -0,0 +1,19 @@
+// We cannot provide stable mock data out of the template.
+// If you introduce mock data, by adding .json files in your webapp/localService/mockdata folder you have to provide the following minimum data:
+// * At least 3 A_CustomerReturn in the list
+
+sap.ui.define([
+	"sap/ui/test/Opa5",
+	"./arrangements/Startup",
+	"./MasterJourney",
+	"./NavigationJourney",
+	"./NotFoundJourney",
+	"./BusyJourney"
+], function (Opa5, Startup) {
+	"use strict";
+	Opa5.extendConfig({
+		arrangements: new Startup(),
+		viewNamespace: "ZTEST.MASTER_DETAIL.view.",
+		autoWait: true
+	});
+});
